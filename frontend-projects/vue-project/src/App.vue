94ade7903beb2f61965abf4de81f74e1568db299
christ youzan <christyouzan@christs-MacBook-Air.local>
2024-07-11 14:18:26 +0100

    separated the css accordingly footer, navbar and main content

--- frontend-projects/vue-project/src/App.vue
+++ frontend-projects/vue-project/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBarTemplate.vue'
-// import Footer from '@/components/FooterTemplate.vue'
+import Footer from '@/components/FooterTemplate.vue'
 import '@/assets/weather-app.css'
 import { RouterView } from 'vue-router'
 </script>
@@ -8,7 +8,13 @@ import { RouterView } from 'vue-router'
   <div class="golden_container">
     <NavBar></NavBar>
     <section class="grid_container">
-      <div class="item1"><RouterView /></div>
+      <div class="item1">
+        <div><RouterView /></div>
+        <div class="home_content_card">
+          <span><h1>Content</h1></span>
+          <span><h1>Content</h1></span>
+        </div>
+      </div>
       <div class="item3"><Footer></Footer></div>
     </section>
   </div>
--- frontend-projects/vue-project/src/components/FooterTemplate.vue
+++ frontend-projects/vue-project/src/components/FooterTemplate.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="footer_container"></section>
+  <section class="footer_container">
+    <div class="footer_one"></div>
+    <div class="footer_two"></div>
+    <div class="footer_three"></div>
+  </section>
 </template>
